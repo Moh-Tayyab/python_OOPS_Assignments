bebc027cae3a53853ba93dd201391f21ba42304d
Muhammad-Tayyab <160000753+Moh-Tayyab@users.noreply.github.com>
2025-05-19 15:44:49 +0500

    Implement Product class with property decorators for price management

--- 18_property_decorator.py
+++ 18_property_decorator.py
@@ -2,3 +2,37 @@
 # Assignment:
 # Create a class Product with a private attribute _price. Use @property to get the price, @price.setter to update it, and @price.deleter to delete it.
 
+class Product():
+    def __init__(self, price):
+        self.__price = price
+        
+    @property    
+    def price(self):
+        return f"price {self.__price}"
+        
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            print("price cannot be negative!")  
+        else:
+            print("setting price..")
+            self.__price = value
+            
+    @price.deleter
+    def price(self):
+        print("deleting price..")
+        del self.__price     
+        
+
+p = Product(1000) 
+
+print(p.price)         # ✅ price 1000
+p.price = 1200         # ✅ setting price
+print(p.price)         # ✅ price 1200
+
+p.price = -600         # ❌ price cannot be negative!
+
+del p.price            # ✅ deleting price
+
+
+          
